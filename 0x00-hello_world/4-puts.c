27f1c26800e4243a64fd0bc12740bd2005526c86
csnjagi <csnjagi@gmail.com>
2023-06-15 04:56:49 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-#include<conio.h>
 /**
- * main - Entry point
+ * main - Program that prints with puts function
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
