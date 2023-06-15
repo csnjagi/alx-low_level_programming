bc4cdd5428bbbc9f7a0b14ae9432df57300ae968
csnjagi <csnjagi@gmail.com>
2023-06-15 05:27:25 -0700

    Correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-#include<conio>
 /**
-* main - Entry point
-*
+* main - Program that prints a line using the printf function
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
+
